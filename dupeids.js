187a14417c743ca3270927cc56c6e6169e234240
Tyler Barnes <tylerdbarnes@gmail.com>
2022-11-22 15:03:53 -0800

    report more

--- dupeids.js
+++ dupeids.js
@@ -3,9 +3,28 @@ const { request, gql } = require("graphql-request")
 setImmediate(async () => {
   await reportDuplicateNodesIds({
     nodeType: `ExtensionPlugin`,
+    fieldName: `extensionPlugins`,
     query: gql`
       query NODE_LIST_QUERY($first: Int!, $after: String) {
         extensionPlugins(first: $first, after: $after) {
+          nodes {
+            id
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    `,
+  })
+
+  await reportDuplicateNodesIds({
+    nodeType: `Document`,
+    fieldName: `documents`,
+    query: gql`
+      query NODE_LIST_QUERY($first: Int!, $after: String) {
+        documents(first: $first, after: $after) {
           nodes {
             id
           }
@@ -23,15 +42,25 @@ const reportDuplicateNodesIds = async ({
   query,
   endCursor = null,
   ids = new Set(),
+  endCursors = new Set(),
   dupeCount = 0,
   nodeType = ``,
+  fieldName = ``,
 }) => {
   const data = await request("https://content.wpgraphql.com/graphql", query, {
     first: 20,
     after: endCursor,
   })
 
-  data?.extensionPlugins?.nodes?.forEach((node) => {
+  const newEndCursor = data[fieldName].pageInfo.endCursor
+
+  if (endCursors.has(newEndCursor)) {
+    console.log(`Found duplicate endCursor: ${newEndCursor}`)
+  } else {
+    endCursors.add(newEndCursor)
+  }
+
+  data[fieldName]?.nodes?.forEach((node) => {
     if (ids.has(node.id)) {
       dupeCount++
       console.log(`found duplicate ${nodeType} node with id ${node.id}`)
@@ -40,13 +69,15 @@ const reportDuplicateNodesIds = async ({
     }
   })
 
-  if (data.extensionPlugins?.pageInfo?.hasNextPage) {
+  if (data[fieldName]?.pageInfo?.hasNextPage) {
     await reportDuplicateNodesIds({
       query,
-      endCursor: data.extensionPlugins?.pageInfo?.endCursor,
+      endCursor: data[fieldName]?.pageInfo?.endCursor,
       ids,
       nodeType,
       dupeCount,
+      endCursors,
+      fieldName,
     })
   } else {
     console.log(
